290d36cc5b807925dc6f951327dade181146fa27
Vitaly Kamynin <vitalykamynin@Vitalys-MacBook-Pro.local>
2024-11-13 21:17:21 -0500

    UI_v2

--- src/helpers/seleniumWebDriver.py
+++ src/helpers/seleniumWebDriver.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+
 
 
 class Driver:
     def driver_setup(self):
-        driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
         return driver
 
     def wait_until(self, driver, timeout):
